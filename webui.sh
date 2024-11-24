65b7e170b973bc2e9a3c85c6aaf151b4792c5a65
Chia-Chun Wu <ccwu0918@gmail.com>
2024-11-25 00:44:17 +0800

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -1,7 +1,7 @@
 # If you could not download the model from the official site, you can use the mirror site.
 # Just remove the comment of the following line .
-# 如果你无法从官方网站下载模型，你可以使用镜像网站。
-# 只需要移除下面一行的注释即可。
+# 如果你無法從官方網站下載模型，你可以使用鏡像網站。
+# 只需要移除下面一行的註解即可。
 
 # export HF_ENDPOINT=https://hf-mirror.com
 
